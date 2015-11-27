9f62b688860352ce78b5e5793eee02778e83c329
priyank-purohit <priyankpurohit31@gmail.com>
2015-11-27 01:32:50 -0500

    Separated mpileups folder into locus folders; updated the mpileup_download_by_region.sh file to download mpileups for the 5 genes. Did not run this yet since bedtools part is TO DO as of now

--- cgi-bin/mpileup_download_by_region.sh
+++ cgi-bin/mpileup_download_by_region.sh
@@ -27,8 +27,33 @@ for i in $(cat data/iplant_path_to_rnaseq_bam_files.txt); do # Iterate over the 
 		wget -P mpileups/temp/ $BAI_LINK
 
 		# Get the mpileup
-		echo '>>> GETTING MPILEUPS'
-		samtools mpileup mpileups/temp/$FILE_NAME -d 8000 -r Chr1:3631-5899 > mpileups/$out_file_name
+		echo '>>> GETTING MPILEUPS + READ MAPS'
+
+		# AT1G01010
+		samtools mpileup mpileups/temp/$FILE_NAME -d 8000 -r Chr1:3631-5899 > mpileups/AT1G01010/$out_file_name
+		echo -n $out_file_name >> mpileups/AT1G01010/read_counts_wc_method.txt
+		samtools view mpileups/temp/$FILE_NAME Chr1:3631-5899 | wc -l >> mpileups/AT1G01010/read_counts_wc_method.txt
+		
+		# AT2G24270
+		samtools mpileup mpileups/temp/$FILE_NAME -d 8000 -r Chr2:10327050-10329941 > mpileups/AT2G24270/$out_file_name
+		echo -n $out_file_name >> mpileups/AT2G24270/read_counts_wc_method.txt
+		samtools view mpileups/temp/$FILE_NAME Chr1:3631-5899 | wc -l >> mpileups/AT2G24270/read_counts_wc_method.txt
+		
+		# AT3G24650
+		samtools mpileup mpileups/temp/$FILE_NAME -d 8000 -r Chr3:8997370-9001063 > mpileups/AT3G24650/$out_file_name
+		echo -n $out_file_name >> mpileups/AT3G24650/read_counts_wc_method.txt
+		samtools view mpileups/temp/$FILE_NAME Chr1:3631-5899 | wc -l >> mpileups/AT3G24650/read_counts_wc_method.txt
+		
+		# AT3G24660
+		samtools mpileup mpileups/temp/$FILE_NAME -d 8000 -r Chr3:9003576-9005943 > mpileups/AT3G24660/$out_file_name
+		echo -n $out_file_name >> mpileups/AT3G24660/read_counts_wc_method.txt
+		samtools view mpileups/temp/$FILE_NAME Chr1:3631-5899 | wc -l >> mpileups/AT3G24660/read_counts_wc_method.txt
+		
+		# AT5G66460
+		samtools mpileup mpileups/temp/$FILE_NAME -d 8000 -r Chr5:26538395-26541036 > mpileups/AT5G66460/$out_file_name
+		echo -n $out_file_name >> mpileups/AT5G66460/read_counts_wc_method.txt
+		samtools view mpileups/temp/$FILE_NAME Chr1:3631-5899 | wc -l >> mpileups/AT5G66460/read_counts_wc_method.txt
+		
 
 		# Delete the downloaded BAM and BAI files
 		echo '>>> DELETING BOTH FILES.'
